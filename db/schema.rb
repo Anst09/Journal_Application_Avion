50e6b243a817794950d036ba5ee01295b4aa0fb9
Anst09 <anp.obras97@gmail.com>
2024-06-15 11:46:15 +0800

    testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_110810) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_14_142347) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -23,6 +25,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_110810) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.integer "user_id", null: false
+    t.index ["category_id"], name: "index_tasks_on_category_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_110810) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "tasks", "categories"
+  add_foreign_key "tasks", "users"
 end
